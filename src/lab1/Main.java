25ef37f9ba1fa18d182115d898d64593c0060cd3
William Song <vanish25th@gmail.com>
2025-09-08 09:57:24 -0400

    we ball, got to task 3.4

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+import java.awt.event.FocusAdapter;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(args);
+
+    }
+}
